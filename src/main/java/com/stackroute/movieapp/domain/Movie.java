de306152ee509edf561f851c13ed36603f2cc6c7
sansreessr <sreenathsankarssr@gmail.com>
2019-10-04 10:15:31 +0530

    lombok added

--- src/main/java/com/stackroute/movieapp/domain/Movie.java
+++ src/main/java/com/stackroute/movieapp/domain/Movie.java
@@ -1,6 +1,7 @@
 package com.stackroute.movieapp.domain;
 
 import lombok.AllArgsConstructor;
+import lombok.Builder;
 import lombok.Data;
 import lombok.NoArgsConstructor;
 
@@ -11,6 +12,7 @@ import javax.persistence.Id;
 @Data
 @NoArgsConstructor
 @AllArgsConstructor
+@Builder
 public class Movie {
 
     @Id
